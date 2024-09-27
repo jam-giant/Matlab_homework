ce4a7a05224c6863281532c6ccb28f357dbdbf59
jam-giant <chideraanya@icloud.com>
2024-09-27 14:31:19 +0000

    Finished Task_1.m

---
+++ Assignment_1/Task_1.m
@@ -0,0 +1,8 @@
+radius = 5;
+%The equation for area
+area = pi * radius * radius; 
+fprintf('The area of the circle is: %g\n', area) %After the area calculation is made, this line prints the result of the calculation. 
+%Equation for Circumference
+circumference = 2 * pi * radius; 
+%Prints the result of the circumference calculation
+fprintf('The circumference of the circle is: %g', circumference) git 
